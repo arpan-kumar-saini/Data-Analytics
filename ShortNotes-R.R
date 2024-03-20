5ff478689a199bfec59a90abbf2434d887f5d0dd
Arpan <arpan.it.21019@recb.ac.in>
2024-03-20 21:20:32 +0530

    Add files via upload

---
+++ ShortNotes-R.R
@@ -0,0 +1,28 @@
+# # R language 
+# 1. lang basics{
+#         1.variables
+#         2.data-types
+#         3.type converting
+#         4.operators
+#         5.Conditional Statements{
+#           1. If Else
+#           2. switch
+#         }
+#         6. Next and Break statements
+#         7.loops{
+#           1. For loop
+#           2. While Loop
+#           3. Repeat loop
+#         }
+#         8. Taking Input
+#         9.Function
+#         10.Built-In Function
+#     }
+# 2. dsa {
+#       1. Vector
+#       2. List
+#       3.Array
+#       4.Matrix
+#       5.Data Frames
+#       6.Factors
+# }
